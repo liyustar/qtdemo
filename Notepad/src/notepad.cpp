12c18078ed8bd237fb6bb767c0a5d4227d202f09
liyustar <liyustar@gmail.com>
2015-02-09 15:34:23 +0800

    can open file" ;

--- Notepad/src/notepad.cpp
+++ Notepad/src/notepad.cpp
@@ -1,6 +1,10 @@
 #include "notepad.h"
 #include "ui_notepad.h"
 
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+
 Notepad::Notepad(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Notepad)
@@ -20,7 +24,19 @@ void Notepad::on_quitButton_clicked()
 
 void Notepad::on_actionOpen_triggered()
 {
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
+            tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
 
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could note open file"));
+            return;
+        }
+        QTextStream in(&file);
+        ui->textEdit->setText(in.readAll());
+        file.close();
+    }
 }
 
 void Notepad::on_actionSave_triggered()
