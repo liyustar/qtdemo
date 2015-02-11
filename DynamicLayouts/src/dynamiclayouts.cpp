c75bda58bf536c495a48ec6caf5b8ea7962e2919
liyustar <liyustar@gmail.com>
2015-02-11 15:14:13 +0800

    finish dynamiclayouts

--- DynamicLayouts/src/dynamiclayouts.cpp
+++ DynamicLayouts/src/dynamiclayouts.cpp
@@ -2,16 +2,17 @@
 
 #include "dynamiclayouts.h"
 
-#define HELP_TEXT "This widget takes up all the remaining " \
-    "space in the top-level layout."
+#define HELP_TEXT "This example shows how to change layouts dynamically."
 
 DynamicLayouts::DynamicLayouts(QWidget *parent)
     : QDialog(parent)
 {
     createOptionsGroupBox();
     createButtonBox();
+    createRotableGroupBox();
 
     mainLayout = new QGridLayout;
+    mainLayout->addWidget(rotableGroupBox, 0, 0);
     mainLayout->addWidget(optionsGroupBox, 1, 0);
     mainLayout->addWidget(buttonBox, 2, 0);
     setLayout(mainLayout);
@@ -59,20 +60,52 @@ void DynamicLayouts::buttonsOrientationChanged(int index) {
 }
 
 void DynamicLayouts::rotateWidgets() {
+
+    // 将控件从布局上移除
+    foreach (QWidget *widget, rotableWidgets)
+        rotableLayout->removeWidget(widget);
+
+    rotableWidgets.enqueue(rotableWidgets.dequeue());
+
+    // 将控件从新加入到布局中
+    const int n = rotableWidgets.count();
+    for (int i = 0; i < n / 2; ++i) {
+        rotableLayout->addWidget(rotableWidgets[n - i - 1], 0, i);
+        rotableLayout->addWidget(rotableWidgets[i], 1, i);
+    }
 }
 
 void DynamicLayouts::help() {
+    QMessageBox::information(this, tr("Dynamic Layouts Help"),
+            tr(HELP_TEXT));
 }
 
 void DynamicLayouts::createRotableGroupBox() {
+    rotableGroupBox = new QGroupBox(tr("Rotable Widgets"));
 
+    rotableWidgets.enqueue(new QSpinBox);
+    rotableWidgets.enqueue(new QSlider);
+    rotableWidgets.enqueue(new QDial);
+    rotableWidgets.enqueue(new QProgressBar);
+
+    for (int i = 0; i < 4; ++i) {
+        connect(rotableWidgets[i], SIGNAL(valueChanged(int)),
+                rotableWidgets[(i + 1) % 4], SLOT(setValue(int)));
+    }
+
+    rotableLayout = new QGridLayout;
+    rotableGroupBox->setLayout(rotableLayout);
+
+    rotateWidgets();
 }
 
 void DynamicLayouts::createOptionsGroupBox() {
+    // 控件组的框框
     optionsGroupBox = new QGroupBox(tr("Options"));
 
     buttonsOrientationLabel = new QLabel(tr("Orientation of buttons:"));
 
+    // 下拉列表
     buttonsOrientationComboBox = new QComboBox;
     buttonsOrientationComboBox->addItem(tr("Horizontal"), Qt::Horizontal);
     buttonsOrientationComboBox->addItem(tr("Vertical"), Qt::Vertical);
@@ -87,11 +120,14 @@ void DynamicLayouts::createOptionsGroupBox() {
 }
 
 void DynamicLayouts::createButtonBox() {
+    // 对话框的标准按钮
     buttonBox = new QDialogButtonBox;
 
     closeButton = buttonBox->addButton(QDialogButtonBox::Close);
     helpButton = buttonBox->addButton(QDialogButtonBox::Help);
     rotateWidgetsButton = buttonBox->addButton(tr("Rotate &Widgets"), QDialogButtonBox::ActionRole);
 
-    // add connect
+    connect(rotateWidgetsButton, SIGNAL(clicked()), this, SLOT(rotateWidgets()));
+    connect(helpButton, SIGNAL(clicked()), this, SLOT(help()));
+    connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
 }
