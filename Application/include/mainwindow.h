5c8cc5e410ab2cba8f33b45390aff6e38fe1363b
liyustar <liyustar@gmail.com>
2015-02-11 17:29:47 +0800

    add Menu Bar

--- Application/include/mainwindow.h
+++ Application/include/mainwindow.h
@@ -3,11 +3,35 @@
 
 #include <QMainWindow>
 
+class QAction;
+class QMenu;
+class QPlainTextEdit;
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
     public:
         MainWindow();
+
+    private:
+        void createActions();
+        void createMenus();
+
+        QPlainTextEdit *textEdit;
+
+        QMenu *fileMenu;
+        QMenu *editMenu;
+        QMenu *helpMenu;
+        QAction *newAct;
+        QAction *openAct;
+        QAction *saveAct;
+        QAction *saveAsAct;
+        QAction *exitAct;
+        QAction *cutAct;
+        QAction *copyAct;
+        QAction *pasteAct;
+        QAction *aboutAct;
+        QAction *aboutQtAct;
 };
 
 #endif // MAINWINDOW_H_
